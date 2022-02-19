319641743ff76843adfe43e2ab08d7bcfb5ceb81
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2022-02-19 15:18:16 +0530

    matrix problem added

---
+++ matrix/rowWithMax1.cpp
@@ -0,0 +1,43 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ 
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	   int row=0;
+	   int col=m-1;
+	   int maxrow=-1;
+	   while(row<n && col>=0){
+	       if(arr[row][col]==1){
+	           maxrow=row;
+	           col--;
+	       }else
+	       row++;
+	   }
+	  return maxrow; 
+}
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
